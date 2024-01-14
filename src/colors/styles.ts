b0f33f91056e267033564e11f48e3647bf65ec62
Kayo Silva <91280312+KayoSilva19@users.noreply.github.com>
2024-01-14 15:52:29 -0300

    :sparkles: Organizacao

---
+++ src/colors/styles.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  gray100: '#f2f2f2',
+  gray200: '#d9d9d9',
+  gray300: '#7f7f7f',
+  gray400: '#333333',
+  gray500: '#262626',
+  gray600: '#191919',
+  gray700: '#0c0c0c',
+  purple: '#8284fa',
+  purpleDark:'#5e60ce',
+  blue: '#4ea8de',
+  blueDark: '#1d6e9f',
+  danger: '#e25858',
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native"
+import { colors } from '../../colors/styles'
+
+export const styles = StyleSheet.create({
+  content: {
+    flex:1,
+    backgroundColor: colors.purple
+  },
+})
